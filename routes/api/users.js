df828484e7e180a4c5cb2ed9921ccb39a0a17df6
farhan2742 <farhan2742@gmail.com>
2020-11-29 18:24:10 +0500

    Added User Authenication

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
 
 // Load User model
 const User = require('../../models/User')
@@ -65,8 +68,26 @@ router.post('/login', (req, res) => {
             bcrypt.compare(password, user.password)
                 .then(isMatch => {
                     if(isMatch) {
-                        res.json({
-                            msg: 'Success'
+                        // User Matched
+                        
+                        // Create JWT payload
+
+                        const payload = {
+                            id: user.id,
+                            name: user.name,
+                            avatar: user.avatar
+                        }; 
+
+                        // Sign Token
+                        jwt.sign(
+                            payload, 
+                            process.env.secret, 
+                            { expiresIn : 3600 }, 
+                            (err, token) => {
+                                res.json({
+                                    success: true,
+                                    token: 'Bearer ' + token
+                                })
                         });
                     } else {
                         return res.status(400).json({
